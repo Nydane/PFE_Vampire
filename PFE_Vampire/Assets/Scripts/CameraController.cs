d950611672a85351b4df0f8c010f177032682030
Maxime Moneyron <maxime.moneyron@gmail.com>
2020-03-25 20:01:39 +0100

    [CharacterController]

---
+++ PFE_Vampire/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Camera Parameters")]
+    public Transform targetFocus;
+
+    [Range(0.0f, 1.0f)]
+    public float lerpPower = .5f;
+
+    public float distance = 5.0f;
+
+    public float verticalOffset = 1.0f;
+    public float horizontalOffset = 2.0f;
+
+    [Range(-90.0f, 90.0f)]
+    public float verticalAngle = 0.0f;
+    [Range(-90.0f, 90.0f)]
+    public float horizontalAngle = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vOffset = Vector3.up * verticalOffset;
+        Vector3 hOffset = Vector3.right * horizontalOffset;
+        Vector3 dist = Vector3.forward * -distance;
+
+        transform.position = targetFocus.position + dist + vOffset + hOffset;
+    }
+}
---
+++ PFE_Vampire/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0;
+    public Rigidbody rb;
+    public float jumpVelocity;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.MovePosition(transform.position + Vector3.right * -speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
+        }
+
+        //Jump
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jumpVelocity;
+            
+        }
+       
+    }
+}
